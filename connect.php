f64d8a3d06bb880026868bf0b2bb0f0a7a3406f0
billxcode <xcodebill@gmail.com>
2016-11-24 01:14:07 +0700

    spa vue 1.0

---
+++ connect.php
@@ -0,0 +1,46 @@
+<?php 
+
+if (isset($_GET['section'])) {
+	$section = $_GET['section'];
+	$data = file_get_contents('http://api-1cak.herokuapp.com/'.$section);
+	echo $data;
+}
+
+if (isset($_GET['param']) && isset($_GET['next'])) {
+	$section = $_GET['param'];
+	$next = $_GET['next'];
+	$data = file_get_contents('http://api-1cak.herokuapp.com/'.$section.'/'.$next);
+	echo $data;	
+}
+
+if(isset($_GET['search'])){
+	$keyword = $_GET['search'];
+	$data = file_get_contents('http://api-1cak.herokuapp.com/search?q='.$keyword);
+	echo $data;
+}
+
+if (isset($_GET['url'])) {
+	$url = $_GET['url'];
+	if(urlExists($url)){
+		echo "success";
+	}else{
+		echo "Failed";
+	}
+}
+
+function urlExists($url=NULL)  
+{ 
+    if($url == NULL) return false;  
+    $ch = curl_init($url);  
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $data = curl_exec($ch);  
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);  
+    curl_close($ch);  
+    if($httpcode>=200 && $httpcode<300){  
+        return true;  
+    } else {  
+        return false;  
+    }  
+} 
+
+ ?>
---
+++ main.js
@@ -0,0 +1,99 @@
+var vm = new Vue({
+  el:"#wrapper",
+  data:{
+      trolldata:[],
+      section:"lol",
+      load:true,
+      next:"",
+      busy:true,
+      keyword:""
+  },
+  beforeCompile: function () {
+    this.load = true;
+      // this.scrolled();
+      this.getData();
+  },
+  methods:{
+    search:function(){
+      this.load = true;
+          this.$http.get('/connect.php?search='+this.keyword).then((response)=>{
+            console.log(JSON.parse(response.body));
+
+            trolldata = JSON.parse(response.body);
+            page = trolldata.page;
+            this.next = page.next;
+            post = trolldata.posts; 
+            for (var i = 0; i < post.length; i++) {
+              this.$http.get('/connect.php?url='+post[i].url).then((response)=>{
+                  if (response.status==200) {
+                    console.log(response.status);
+                  }
+              });
+            }
+            this.trolldata = post;
+            this.load = false;
+          });
+    },
+    sortenString:function(data){
+      return data.substr(0,30);
+    },
+    scrolled:function(){
+      var vm = this;
+      var lock = true;
+      var section = this.section;
+      var next = this.next;
+
+          vm.$http.get('/connect.php?param='+section+'&next='+next).then((response)=>{
+
+            trolldata = JSON.parse(response.body);
+            page = trolldata.page;
+            this.next = page.next;
+            post = trolldata.posts; 
+            for (var i = 0; i < post.length; i++) {
+              this.$http.get('/connect.php?url='+post[i].url).then((response)=>{
+                  if (response.status==200) {
+                    console.log(response.status);
+                  }
+              });
+            }
+            this.trolldata.push(post);
+            console.log(this.trolldata);
+            this.load = false;
+          });
+  
+    },
+    setData:function(data){
+      this.load = true;
+      this.section = data;
+      this.getData();
+    },
+    getData:function(){
+          this.$http.get('/connect.php?section='+this.section).then((response)=>{
+            console.log(JSON.parse(response.body));
+
+            trolldata = JSON.parse(response.body);
+            page = trolldata.page;
+            this.next = page.next;
+            post = trolldata.posts; 
+            for (var i = 0; i < post.length; i++) {
+              this.$http.get('/connect.php?url='+post[i].url).then((response)=>{
+                  if (response.status==200) {
+                    console.log(response.status);
+                  }
+              });
+            }
+            this.trolldata = post;
+            this.load = false;
+          });
+         
+    }
+  }
+
+});
+
+
+      $(window).scroll(function(){
+        if ($(window).scrollTop() == $(document).height()-$(window).height()){
+          vm.scrolled();
+        }
+      });
