a7482329a945f3fb237fd5833de444b2483821e3
billxcode <xcodebill@gmail.com>
2016-12-22 22:31:14 +0700

    try to repair blocked mix content by changing protocol to www

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var vm = new Vue({
       next:"",
       busy:true,
       keyword:"",
-      host:"//bill.hol.es/project/1cak/", //this belongs your host
+      host:"www.bill.hol.es/project/1cak/", //this belongs your host
   },
   beforeCompile: function () {
     this.load = true;
